ed74ed823fb6770eca04227d703f9080a26e93e7
Dan <drbrown01@yahoo.com>
2017-10-26 18:49:48 -0500

    added updated excercise files

--- TodayWithSolutions/06-HandlebarsAnimals/Unsolved/server.js
+++ TodayWithSolutions/06-HandlebarsAnimals/Unsolved/server.js
@@ -35,12 +35,13 @@ app.get("/dog", function(req, res) {
   // Handlebars requires an object to be sent to the dog.handlebars file. Lucky for us, animals[0] is an object!
 
   // 1. Send the dog object from the animals array to the dog.handlebars file.
-
+  res.render("dog", animals[0]);
 });
 
 app.get("/all-pets", function(req, res) {
   // Handlebars requires an object to be sent to the index.handlebars file.
-
+var currentAnimal = animals[i];
+  if (currentAnimal)
   // 2. Send the animals to the index.handlebars file. Remember that animals is an array and not an object.
 
 });
